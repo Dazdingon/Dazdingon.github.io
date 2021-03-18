db5b646690605095b076db0acfbc378ff4db6790
Dazdingon <nlareman@gmail.com>
2021-03-18 10:24:51 +0200

    pose problem

---
+++ onscreen/everypixel.js
@@ -0,0 +1,22 @@
+var width;
+var hight;
+
+/*
+the value we want = cos(angle)
+._____.               
+._____.     .
+|    /      |
+|   /       |
+|  /        | 1
+| /         |
+|/ angle    |
+'           '
+
+*/
+
+/*
+
+get the Y angle of every pixel's X
+        X                        Y
+
+*/
